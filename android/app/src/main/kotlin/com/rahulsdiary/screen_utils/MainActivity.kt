8b731750582058bcfe439563a795cc249ca0fc68
Md. Rahul Reza <git-rahul@devxhub.com>
2023-03-30 10:23:38 +0600

    first test

---
+++ android/app/src/main/kotlin/com/rahulsdiary/screen_utils/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.rahulsdiary.screen_utils
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:screen_utils/conts/constant.dart';
+import 'package:screen_utils/pages/login_page.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+          child: Container(
+            padding: kPaddingMargin,
+            width: double.infinity,
+            height: double.infinity,
+            color: const Color.fromARGB(255, 19, 143, 120),
+            child: Column(children: [
+              Image.asset('images/welcome.png'),
+              SizedBox(
+                height: 10.h,
+              ),
+              Text(
+                'Let’s Get '
+                    'Started',
+                style: textTheme.headline1,
+              ),
+              SizedBox(
+                height: 10.h,
+              ),
+              Text(
+                  'Connect with each other while chatting or '
+                      'calling. Enjoy safe and private texting',
+                  style: textTheme.subtitle1),
+              SizedBox(height: 20.h,),
+              Container(
+                width: double.infinity,
+                height: 40,
+                child: ElevatedButton(onPressed: (){
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>LogINPage()));
+                }, child: Text('Join Now'),
+                  style:btnStyle(const Color.fromARGB(255, 19, 143, 120), Colors.white),
+                ),
+              ),
+              SizedBox(height: 30.h,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Already have an account?',
+                    style: textTheme.subtitle1,
+                  )
+                ],)
+            ]),
+          ),
+        ));
+  }
+}
